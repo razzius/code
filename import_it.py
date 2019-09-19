d887d8b96fa678102b2c05eeb9bc23a8ca13d174
Razzi Abuissa <razzi53@gmail.com>
2019-09-19 01:30:56 -0700

    Try to match on existing import

--- import_it.py
+++ import_it.py
@@ -12,25 +12,34 @@ symbol = args.symbol
 repository = args.repository
 
 if re.fullmatch('[A-Z_]+', symbol):
-    search = rf'{symbol}\s=\s'
+    search = rf'^{symbol}\s=\s'
 elif symbol[0].isupper():
-    search = f'class {symbol}'
+    search = rf'class {symbol}\('
 else:
-    search = f'def {symbol}'
+    search = rf'def {symbol}\('
 
-result = Ripgrepy(search, repository).json().run().as_dict[0]
+result = Ripgrepy(search, repository).json().run().as_dict
 
-absolute_path = result['data']['path']['text']
+def try_to_match_on_existing_import(symbol, repository):
+    search = rf'import \b{symbol}\b'
+    result = Ripgrepy(search, repository).json().run().as_dict
+    import_source = result[0]['data']['lines']['text'].rstrip()
+    return import_source
 
+if not result:
+    import_source = try_to_match_on_existing_import(symbol, repository)
+else:
+    absolute_path = result[0]['data']['path']['text']
 
-def trim_prefix(s, prefix):
-    if s.startswith(prefix):
-        return s[len(prefix):]
-    return s
+    def trim_prefix(s, prefix):
+        if s.startswith(prefix):
+            return s[len(prefix):]
+        return s
 
+    relative_path = trim_prefix(absolute_path, repository)
 
-relative_path = trim_prefix(absolute_path, repository)
+    import_path = trim_prefix(relative_path, '/').replace('.py', '').replace('/', '.')
 
-import_path = trim_prefix(relative_path, '/').replace('.py', '').replace('/', '.')
+    import_source = f'from {import_path} import {symbol}'
 
-print(f'from {import_path} import {symbol}')
+print(import_source)
